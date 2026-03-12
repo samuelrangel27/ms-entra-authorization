85b18f8b58a315cfc2a768347323e71ee0245766
samuel rangel <samuelrangel27@gmail.com>
2026-03-12 12:51:44 -0600

    feat: Configure Azure AD B2C authentication, enforce Authorization Code flow, and add reverse proxy support.

--- roll-dice/RollDice.Mvc/Program.cs
+++ roll-dice/RollDice.Mvc/Program.cs
@@ -4,12 +4,32 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+    .AddMicrosoftIdentityWebApp(builder.Configuration, "AzureAd");
+
+// Configure forwarded headers so the app knows its public URL when running behind Azure's reverse proxy
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    // These clear the trusted proxies so it accepts Azure's proxy headers
+    options.KnownIPNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
+// Force the OpenID Connect middleware to use the Authorization Code flow
+builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.ResponseType = "code";
+
+    // Request an access token in addition to the id_token
+    // In B2C, you need to request your own ClientId as a scope, or another explicitly defined scope
+    options.Scope.Add(builder.Configuration.GetSection("AzureAd")["ClientId"]!);
+});
 
 builder.Services.AddControllersWithViews(options =>
 {
@@ -22,6 +42,8 @@ builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
